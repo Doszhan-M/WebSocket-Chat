b7695cebc7e1d4834f1214d6c85d91a0d06a1294
Doszhan <Doszhan.m@mail.ru>
2021-07-17 00:15:34 +0600

    add host

--- ChatRoom/Client/chat/js/all_rooms.js
+++ ChatRoom/Client/chat/js/all_rooms.js
@@ -14,7 +14,8 @@ window.addEventListener('load', () => {
     }
     var csrftoken = getCookie('csrftoken'); 
 
-    const host = 'http://127.0.0.1:8000/'
+    const host = 'http://195.149.87.163:8000/'
+    // const host = 'http://127.0.0.1:8000/'
     let resultNode = document.querySelector('.slaider')
     let myRoom  // переменная для пункта 5
 
