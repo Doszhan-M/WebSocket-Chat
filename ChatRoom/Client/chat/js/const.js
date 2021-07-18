624843caa064c98de62556ba74ea7afe0577ba0c
Doszhan <Doszhan.m@mail.ru>
2021-07-19 00:13:20 +0600

    edit host

--- ChatRoom/Client/chat/js/const.js
+++ ChatRoom/Client/chat/js/const.js
@@ -1,6 +1,8 @@
 // export const host = 'http://127.0.0.1:8000/'
 export const host = 'http://195.149.87.162:7000/'
-console.log('host', window.location.host)
+export const host1 = 'http://' + window.location.host + '/'
+console.log('host1', host1)
+console.log(typeof(host1))
 
 // Получить токен и объявить переменные________________________________________________________________
 function getCookie(name) {
