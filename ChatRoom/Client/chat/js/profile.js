273a801f16359e713d264421f95015ccdf3c2c0c
Doszhan <Doszhan.m@mail.ru>
2021-07-18 12:49:36 +0600

    add host

--- ChatRoom/Client/chat/js/profile.js
+++ ChatRoom/Client/chat/js/profile.js
@@ -28,7 +28,7 @@ async function getAvaImage() {
   if (profile.avatar == null) {
     ava_image.src = "/static/img/256x256/256_1.png"
   } else {
-    imgUrl = profile.avatar.replace(host = host, '/')
+    imgUrl = profile.avatar
     ava_image.src = imgUrl
   }
 }
